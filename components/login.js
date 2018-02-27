c4d58bf8269e75b25350003c16b3bbae5d030811
cituao <jamarquez@uao.edu.co>
2018-02-27 14:08:02 -0500

    Crea page login

---
+++ components/login.js
@@ -0,0 +1,43 @@
+
+export default class LoginForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: '',
+      passwd: ''
+    };
+
+    this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handlePasswdChange = this.handlePasswdChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleEmailChange(event) {
+    this.setState({ email: event.target.value })
+  }
+
+  handlePasswdChange(event) {
+    this.setState({ passwd: event.target.value })
+  }
+  
+  handleSubmit(event) {
+    alert('Usuario login: ' + this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          email:
+          <input type="text" value={this.state.email} onChange={this.handleEmailChange} />
+        </label>
+        <label>
+          passwd:
+          <input type="text" value={this.state.passwd} onChange={this.handlePasswdChange} />
+        </label>
+        <input type="submit" value="Login" />
+      </form>
+    );
+  }
+}
---
+++ pages/login.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import Layout from '../components/layout.js';
+import Login from '../components/login';
+export default class extends React.Component {
+  static async getInitialProps({ req }) {
+    const userAgent = req ? req.headers['user-agent'] : navigator.userAgent
+    return { userAgent }
+  }
+
+  render() {
+    const muiTheme = getMuiTheme({
+      userAgent: this.props.userAgent,
+      });
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <Layout>
+          <Login />
+        </Layout>
+      </MuiThemeProvider>
+   )
+  }
+}
