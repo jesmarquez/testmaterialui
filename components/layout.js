dfc5bd8dc6360b464b48c17da666fb3a7c9f456f
cituao <jamarquez@uao.edu.co>
2018-02-27 11:32:25 -0500

    Cambia grid layout

--- components/layout.js
+++ components/layout.js
@@ -19,22 +19,16 @@ export default class extends React.Component {
               <h1>Jobby</h1>
             </div>
             <div class="col s4"></div>       
-          
           </div>
-
-
           <div class="row">
-            <div class="col s4">
+            <div class="col s1">
             </div>
-            <div class="col s4">
+            <div class="col s10">
               {this.props.children}
             </div>
-            <div class="col s4">
-
+            <div class="col s1">
             </div>
           </div>
-
-
         </div>
       </div>
     )
